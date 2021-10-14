bb41d83a8afeeab0d97123c724e1e1db31f9a036
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-15 00:11:09 +0200

    relative size adjustments

--- lunchmeister.js
+++ lunchmeister.js
@@ -1,9 +1,10 @@
 const circleWidth = 140;
 
 (function () {
-  document.querySelector('html').style.setProperty('--icon-size', circleWidth + 'px');
+  document
+    .querySelector('html')
+    .style.setProperty('--icon-size', circleWidth + 'px');
 })();
-
 (function ($) {
   $.fn.shuffle = function () {
     var allElems = this.get(),
@@ -24,7 +25,6 @@ const circleWidth = 140;
     return $(shuffled);
   };
 })(jQuery);
-
 (function () {
   var initialSetup = function (options) {
     options
@@ -65,14 +65,14 @@ const circleWidth = 140;
       return n * 70 + (Math.floor((n + 5) / 6) + Math.floor(n / 6)) * 5;
     }
 
-    $('polyline').css(
-      '-webkit-transform',
-      'translate(' + trans(num) + 'px,0px)'
-    );
+    const transform = 'translate(' + trans(num) + 'px,0px)';
+    $('polyline')
+      .css('-webkit-transform', transform)
+      .css('transform', transform);
   };
 
   var toTheCircle = function (options) {
-    const defaultSize = 6 * circleWidth
+    const defaultSize = 6 * circleWidth;
     var n, size, gapAngle;
 
     n = options.length;
@@ -85,21 +85,31 @@ const circleWidth = 140;
       size = defaultSize / (1 + 1 / Math.sin(Math.PI / n));
     }
 
-    const translation = (defaultSize - size) / 2
+    const translation = (defaultSize - size) / 2;
+
+    function getTransform(index) {
+      return (
+        'rotate(' +
+        (n - 1 - index) * gapAngle +
+        'deg) translate(0,-' +
+        translation +
+        'px)'
+      );
+    }
 
     options
       .css('height', size)
       .css('width', size)
       .css('top', translation)
       .css('left', translation)
-      .css('-webkit-transform', function (index) {
-        return 'rotate(' + ((n - 1 - index) * gapAngle) + 'deg) translate(0,-' + translation + 'px)';
-      });
+      .css('-webkit-transform', getTransform)
+      .css('transform', getTransform);
   };
 
   var dispense = function (option) {
     option
       .css('-webkit-transform', 'rotate(2000deg) translate(400px, 1500px)')
+      .css('transform', 'rotate(2000deg) translate(400px, 1500px)')
       .one('transitionend', function () {
         option.hide();
       });
@@ -122,6 +132,7 @@ const circleWidth = 140;
 
     container
       .css('-webkit-transform', 'rotate(3600deg)')
+      .css('transform', 'rotate(3600deg)')
       .on('transitionend', function (e) {
         if (e.target == this) {
           $('#winnerMessage').show().children().text(luckyWinnerName);
@@ -138,7 +149,7 @@ const circleWidth = 140;
 
       toTheCircle($('.option.selected'));
 
-      $(document).on('mousewheel', function () {
+      $(document).on('wheel', function () {
         spin($('.options'), $('.option.selected'));
       });
     });
